1f9a49657c961664c90d63f4e72c4e1536860986
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-11 02:52:41 +0000

    [Fiz] case Fizzbuzz 2

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,8 @@ def fizzbuzz():
             print("Fizz ", end="")
         if i % 5 == 0 and i % 3 != 0:
             print("Buzz ", end="")
-        if i % 5 == 0 and i % 3 == 0:
-            print("FizzBuzz ", end="")
         else:
-            print("{} ".format(i), end="")
+            if i % 5 == 0 and i % 3 == 0:
+                print("FizzBuzz ", end="")
+            else:
+                print("{} ".format(i), end="")
