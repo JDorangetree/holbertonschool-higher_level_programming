eb443ad850d1cd91ffb131e70d852ab375cbb004
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-12 12:15:10 -0500

    [Fix] case of len argv = 1

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    sum = int(sys.argv[1]) + int(sys.argv[2])
+    len_argv = len(sys.argv)
+    if len_argv == 1:
+        sum = 0
+    else:
+        sum = int(sys.argv[1]) + int(sys.argv[2])
     print("{}".format(sum))
