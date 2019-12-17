e82a4947a36a929da9fe5dc43352ace5906edb7b
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-17 15:28:16 -0500

    [Feature] upload task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.replace("c", "")
-    new_string = my_string.replace("C", "")
+    slist = []
+    for i in range(0, len(my_string)):
+        slist.append(my_string[i])
+    for j in slist:
+        if j == 'c' or j == 'C':
+            slist.remove(j)
+        new_string = "".join(slist)
     return new_string
