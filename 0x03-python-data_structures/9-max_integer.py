1b53cfd989b66de1612e6a5b285653bc0af42aa8
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-17 20:49:59 -0500

    [Feature] upload task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        ordered_list = sorted(my_list)
+        for i in ordered_list:
+            x = i
+        return i
+    else:
+        return None
