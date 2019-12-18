ec087d0015a6bb7d1eda84a9e14d7fd149ee07d9
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-17 20:28:38 -0500

    [Feature] case empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,8 @@ def multiple_returns(sentence):
     if sentence:
         x = len(sentence)
         y = sentence[0]
-        packed = (x, y)
+    else:
+        y = ""
+        x = 0
+    packed = (x, y)
     return(packed)
