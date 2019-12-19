24cbbb81d395a9a8fd0a22d630d87fbe09de3f69
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-19 13:43:09 -0500

    [Fix]

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix:
-        new_matrix = [[row[i]**2 for i in range(len(matrix))]for row in matrix]
+        new_matrix = [[i**2 for i in row]for row in matrix]
         return new_matrix
-    return matrix
