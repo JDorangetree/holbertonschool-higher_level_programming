a157a76f446265a94e462c9879ccf76034829541
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-19 13:04:34 -0500

    [Fix] pep8 style

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,5 @@
 def square_matrix_simple(matrix=[]):
     if matrix:
         new_matrix = [[row[i]**2 for i in range(len(matrix))]for row in matrix]
-    return new_matrix
+        return new_matrix
+    return matrix
