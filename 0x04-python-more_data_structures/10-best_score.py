7211e033ed0be175d05eb7989eda077baa971906
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-19 22:35:53 -0500

    [Fix] name of key

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,5 +4,6 @@ def best_score(a_dictionary):
         list_values = a_dictionary.values()
         x = max(list_values)
         dict_max = {k: v for (k, v) in a_dictionary.items() if v == x}
-        return dict_max.keys()
+        list_max = list (dict_max)
+        return list_max[0]
     return None
