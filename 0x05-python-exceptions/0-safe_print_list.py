4041822dc4cc760454f6addd4322e2330fcbfb63
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-08 13:09:18 -0500

    [Fix] pep8 style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range (0, x):
+        for i in range(0, x):
             y = my_list[i]
             print("{}".format(y), end="")
-        print("")    
+        print("")
         return x
     except IndexError:
-        for i in range (0, ):
-            y = my_list[i]
-            print("{}".format(y), end="")
-        print("")    
+        print("")
         return i
