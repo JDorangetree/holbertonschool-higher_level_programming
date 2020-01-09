657f5043b706e73e451c0cf3af6739fdc23828e8
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-09 13:27:23 -0500

    [Feature] upload carpet python classes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        x = isinstance(size, int)
+        if not x:
+            print("size must be an integer", end=" ")
+            raise TypeError
+        elif size < 0:
+            print("size must be >= 0", end=" ")
+            raise ValueError
+        else:
+            self.__size = size
