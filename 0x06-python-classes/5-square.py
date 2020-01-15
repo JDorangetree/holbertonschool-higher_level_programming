aadcbfd556dc7e8e93ba60c2c1d65ca741d7463a
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-15 13:05:49 -0500

    [Feature] upload task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        """The __init__ method of the square class
+        Args:
+            size: Is the type int private attribute
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Private instance attribute getter method."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Private instance attribute property setter."""
+        x = isinstance(value, int)
+        if not x:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """method to calculate the area of ​​a square"""
+        a_square = self.__size**2
+        return a_square
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            matrix = [[]]
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print("")
