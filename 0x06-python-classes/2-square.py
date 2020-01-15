672f82b15681191db188151d5e880d5ac86a8f29
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-15 11:26:45 -0500

    [Fix] error message

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,9 @@ class Square:
     """class Square that defines a square"""
     def __init__(self, size=0):
         x = isinstance(size, int)
-        if not x:
-            print("size must be an integer", end=" ")
-            raise TypeError
+        if x == False:
+            raise TypeError("size must be an integer")
         elif size < 0:
-            print("size must be >= 0", end=" ")
-            raise ValueError
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
