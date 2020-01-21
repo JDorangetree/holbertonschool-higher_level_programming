07556c6dd924c2834ae03fbffe2c373bfe95e50b
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-20 21:37:21 -0500

    [Feature] upload task 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_attribute(obj, name, value):
+    """function that adds a new attribute to an object if it’s possible"""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
