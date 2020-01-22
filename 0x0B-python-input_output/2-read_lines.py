2764a3b2358e01bdd6ac709095b76420784ef195
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-21 20:32:50 -0500

    [Feature] upload task 2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    with open(filename, encoding='utf-8') as new_file:
+        i = 0
+        for line in new_file:
+            i = i + 1
+        new_file.seek(0)
+        if nb_lines <= 0:
+            strg = new_file.read()
+            print(strg)
+        elif nb_lines >= i:
+            strg = new_file.read()
+            print(strg)
+        else:
+            new_file.seek(0)
+            while nb_lines > 0:
+                print(new_file.readline(), end="")
+                nb_lines = nb_lines - 1
