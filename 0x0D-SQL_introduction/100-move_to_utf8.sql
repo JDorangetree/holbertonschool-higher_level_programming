ff4f18360d52ae8828b339c538e27dbc78233fad
JDorangetree <julian.naranjo2014@gmail.com>
2020-03-02 19:01:10 -0500

    [Feature] upload task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- converts hbtn_0c_0 database to UTF8 
 --
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8_general_ci;
