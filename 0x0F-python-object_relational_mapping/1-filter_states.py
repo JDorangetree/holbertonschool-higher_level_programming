4c7dad9d3d22052a4d01d6eb53a0d0932f9ce80f
JDorangetree <julian.naranjo2014@gmail.com>
2020-03-28 18:08:10 -0500

    [Fix] order by id

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
                          user=my_name,
                          host="localhost", port=3306)
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id;")
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%' "
+              "ORDER BY states.id ASC;")
     a = c.fetchall()
     for row in a:
         print(row)
