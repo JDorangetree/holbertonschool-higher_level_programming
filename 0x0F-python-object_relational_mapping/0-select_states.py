614529abf8789afc8fc9754a7b90bb0e85606b9e
JDorangetree <julian.naranjo2014@gmail.com>
2020-03-28 16:56:25 -0500

    [Fix] pep8 style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
     my_name = sys.argv[1]
     my_pass = sys.argv[2]
     my_db = sys.argv[3]
-    db = MySQLdb.connect(passwd=my_pass, db=my_db, user=my_name)
+    db = MySQLdb.connect(passwd=my_pass, db=my_db,
+                         user=my_name,
+                         host="localhost", port="3306")
     c = db.cursor()
     c.execute("""SELECT * FROM hbtn_0e_0_usa.states ORDER BY id""")
     a = c.fetchall()
