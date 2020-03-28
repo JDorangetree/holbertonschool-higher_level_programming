60a1c3e2fa213f26a99a6938ffcdcefebfc14af7
JDorangetree <julian.naranjo2014@gmail.com>
2020-03-28 16:43:42 -0500

    [Feature] upload task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    my_name = sys.argv[1]
+    my_pass = sys.argv[2]
+    my_db = sys.argv[3]
+    db = MySQLdb.connect(passwd=my_pass, db=my_db, user=my_name)
+    c = db.cursor()
+    c.execute("""SELECT * FROM hbtn_0e_0_usa.states ORDER BY id""")
+    a = c.fetchall()
+    for row in a:
+        print("{}".format(row))
+    # Close all cursors
+    c.close()
+    # Close all databases
+    db.close()
