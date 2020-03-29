6c48c4dad377cf70da72bea7d351dcb3b6348d6b
JDorangetree <julian.naranjo2014@gmail.com>
2020-03-28 19:08:57 -0500

    [Feature] upload task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""akes in an argument and displays all
+ values in the states table of hbtn_0e_0_usa
+  where name matches the argument."""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    my_name = argv[1]
+    my_pass = argv[2]
+    my_db = argv[3]
+    my_state = argv[4]
+    db = MySQLdb.connect(passwd=my_pass, db=my_db,
+                         user=my_name,
+                         host="localhost", port=3306)
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE states.name = %s"
+              "ORDER BY states.id", (my_state,))
+    a = c.fetchall()
+    for row in a:
+        print(row)
+
+    c.close()
+    db.close()
