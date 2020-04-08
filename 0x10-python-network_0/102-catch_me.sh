1b4409bccc26367d477d7d60865772ebed577fc3
JDorangetree <julian.naranjo2014@gmail.com>
2020-04-07 20:16:03 -0500

    [Fix] url

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that sends a JSON POST request to a URL
-curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98"
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
