d07344697217b1643e2807982086494eff898d76
JDorangetree <julian.naranjo2014@gmail.com>
2020-04-16 20:41:56 -0500

    [Fix] namme error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """sends a request to the URL and displays the body of the response"""
 
-from urllib.request import urlopen
-from sys import argv
-
 if __name__ == "__main__":
+    from urllib.request import urlopen
+    from urllib.error import HTTPError
+    from sys import argv
+
     url = argv[1]
     try:
         with urlopen(url) as response:
             html = response.read()
             print(html.decode('utf-8'))
-    except urllib.error.URLError as e:
+
+    except HTTPError as e:
         print("Error code: {}".format(e.code))
