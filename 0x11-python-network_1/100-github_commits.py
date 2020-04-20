f716146f58a14ff497e1ffe086ef1df2fe9f786d
JDorangetree <julian.naranjo2014@gmail.com>
2020-04-20 14:32:20 -0500

    [Fix] pep8 style

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,11 +10,14 @@ if __name__ == "__main__":
     owner_name = argv[2]
     url = 'https://api.github.com/repos/{}/{}/commits'
     r = requests.get(url.format(owner_name, repo_name))
-    dic = r.json()
     j = 0
-    for i in dic:
-        if (j < 10):
-            name = i.get('commit').get('author').get('name')
-            sha = i.get('commit').get('tree').get('sha')
-            j = j + 1
-            print("{}: {}".format(sha, name))
+    try:
+        dic = r.json()
+        for i in dic:
+            if (j < 10):
+                name = i.get('commit').get('author').get('name')
+                sha = i.get('commit').get('tree').get('sha')
+                j = j + 1
+                print("{}: {}".format(sha, name))
+    except ValueError:
+        print("None")
