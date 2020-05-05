4e056b117a72293e3a12049c52749ff28c8a462b
JDorangetree <julian.naranjo2014@gmail.com>
2020-05-05 18:34:31 -0500

    [Feature] upload task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+const nArgs = args.length;
+const X = 'X';
+let square;
+if (nArgs === 2) {
+  console.log('Missing size');
+} else {
+  square = args[2];
+  while (square > 0) {
+    console.log(X.repeat(args[2]));
+    square--;
+  }
+}
