7e98585daec60dd4a74696251e84bd5c32fe11e0
JDorangetree <julian.naranjo2014@gmail.com>
2020-05-11 22:41:21 -0500

    [Feature] upload task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const ocurreList = [];
+const returnDict = [];
+for (const userId in dict) {
+  ocurreList.push(dict[userId]);
+}
+const uniOcurr = Array.from(new Set(ocurreList));
+for (let i = 0; i < uniOcurr.length; i++) {
+  const listValue = [];
+  for (const key in dict) {
+    if (uniOcurr[i] === dict[key]) {
+      listValue.push(key);
+    }
+  }
+  returnDict.push(uniOcurr[i].toString(), listValue);
+}
+console.log(returnDict);
