eef7299aa21232811372d7e308cb759617c0aefa
JDorangetree <julian.naranjo2014@gmail.com>
2020-05-12 12:14:24 -0500

    [Fix] utf-8

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const args = process.argv;
 
-fs.writeFile(`${args[2]}`, `${args[3]}\n`, error => {
+fs.writeFile(`${args[2]}`, `${args[3]}\n`, 'utf-8', error => {
   if (error) {
     console.log(error);
   }
