3ada364c443ab3541baa0de477b20be18f861aa5
JDorangetree <julian.naranjo2014@gmail.com>
2020-05-12 15:41:22 -0500

    [Feature] upload task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require('request');
+const args = process.argv;
+
+const options = {
+  url: args[2],
+  method: 'GET'
+};
+request(options, function (error, response, body) {
+  if (!error === null) {
+    console.error(error);
+  } else {
+    let count = 0;
+    let result = [];
+    const json = JSON.parse(body);
+    result = json.results;
+    for (let i = 0; i < result.length; i++) {
+      let characters = [];
+      characters = (result[i].characters);
+      for (let a = 0; a < characters.length - 1; a++) {
+        if (characters[a] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
