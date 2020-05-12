c9297fe761c267c5d5c7236d3ac4e310783e6e58
JDorangetree <julian.naranjo2014@gmail.com>
2020-05-12 18:17:40 -0500

    [Feature] upload task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+const request = require('request');
+const args = process.argv;
+const returnDict = {};
+let count = 0;
+const options = {
+  url: args[2]
+};
+request(options, function (error, response, body) {
+  if (!error === null) {
+    console.log(error);
+  } else {
+    const json = JSON.parse(body);
+    const ids = [];
+    for (let i = 0; i < json.length; i++) {
+      ids.push(json[i].userId);
+    }
+    const uniOcurr = Array.from(new Set(ids));
+    for (let b = 0; b < uniOcurr.length; b++) {
+      count = 0;
+      for (let a = 0; a < json.length; a++) {
+        if (json[a].userId === uniOcurr[b]) {
+          if (json[a].completed === true) {
+            count++;
+          }
+        }
+      }
+      returnDict[uniOcurr[b]] = count;
+    }
+    console.log(returnDict);
+  }
+});
