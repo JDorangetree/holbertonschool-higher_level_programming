afb413d5d69a30db78ae50048030476f3c4a0fcc
JDorangetree <julian.naranjo2014@gmail.com>
2020-05-12 19:41:11 -0500

    [Fix] right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -12,7 +12,7 @@ request(options, function (error, response, body) {
   } else {
     const json = JSON.parse(body);
     results = json.results;
-    urlList = results[args[2] - 1].characters;
+    urlList = results[args[2]].characters;
     for (let i = 0; i < urlList.length; i++) {
       request(urlList[i], function (error, response, body) {
         if (!error === null) {
